307d9675007c9cf1ae58128b494df2ff5f67cf9b
d4rkmoons <arifdian0384@gmail.com>
2020-05-06 21:10:28 +0700

    add modal on admin

--- application/views/admin/admin.php
+++ application/views/admin/admin.php
@@ -18,7 +18,9 @@
     <div class="container border">
         <h3>Data</h3>
         <div class="pb-3">
-            <button class="btn btn-info">Tambah</button>
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">Open </button>
+
+            <!-- <button id="tambah" class="btn btn-info">Tambah</button> -->
         </div>
         <br>
         <div class="container-fluid">
@@ -35,12 +37,41 @@
                 </tbody>
             </table>
         </div>
-
     </div>
 </div>
 
-<link rel="stylesheet" href="<?php echo base_url("assets/css/style4.css") ?>">
+<!-- Modal Add -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">New message</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form>
+                    <div class="form-group">
+                        <label for="recipient-name" class="col-form-label">Recipient:</label>
+                        <input type="text" class="form-control" id="recipient-name">
+                    </div>
+                    <div class="form-group">
+                        <label for="message-text" class="col-form-label">Message:</label>
+                        <textarea class="form-control" id="message-text"></textarea>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Send message</button>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Modal Add -->
 
+<link rel="stylesheet" href="<?php echo base_url("assets/css/style4.css") ?>">
 <script type="text/javascript">
     $(document).ready(function () {
         var table = $('#mytable').DataTable();
@@ -50,3 +81,10 @@
         });
     });
 </script>
+<style>
+    #content{
+        width: 100%;
+    }
+</style>
+<!-- Modal -->
+<script src="<?php echo base_url("assets/js/admin.admin.js") ?>"></script>
