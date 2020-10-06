c10533cf862355910ef3fbaa673dff4793915bce
Mark Yeaah <arifdian0384@gmail.com>
2020-10-06 10:36:35 +0700

    kerja di search

--- assets/js/welcome.js
+++ assets/js/welcome.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
             // console.log(r)
             r.forEach(e => {
                 // console.log(e.id_destination)
-                var html = '<div class="col-lg-3 col-sm-6 mb-5">'
+                var html = ' <div class="col-lg-3 col-sm-6 mb-5">'
 				+'<div class="image-tour position-relative">'
 					+'<img src="/assets/upload/'+e.image+'" alt="" class="img-fluid" />'
 				+'</div>'
@@ -31,8 +31,42 @@ $(document).ready(function () {
 
     // 
 
-    $(".btngoes").on("click", function () {
-        console.log("yeah")
-    });
+    // $(".btngoes").on("click", function () {
+    //     console.log("yeah")
+    // });
 
+    $("#cari").on("keypress", function (e) {
+        if (e.which == 13) {
+            $("#datahere").empty();
+            $.ajax({
+                type: "POST",
+                url: url+"/welcome/cari_xhr",
+                data: {
+                    cari: $(this).val()
+                },
+                dataType: "JSON",
+                success: function (r) {
+                    // console.log(r)
+                    r.forEach(e => {
+                        // console.log(e.id_destination)
+                        var html = ' <div class="col-lg-3 col-sm-6 mb-5">'
+                        +'<div class="image-tour position-relative">'
+                            +'<img src="/assets/upload/'+e.image+'" alt="" class="img-fluid" />'
+                        +'</div>'
+                        +'<div class="package-info">'
+                            +'<h6 class="mt-1"><span class="fa fa-map-marker mr-2">  </span>.</h6>'
+                            +'<h5 class="my-2"> '+e.nama_dest+' </h5>'
+                            +'<p class=""> '+e.artikel+' </p>'
+                            +'<ul class="listing mt-3">'
+                            +'<a target="_blank" href="'+e.urlgmaps+'" class="btn btn-info">Go'
+                            +'</ul>'
+                        +'</div>'
+                        +'</div>';
+                        
+                        $("#datahere").append(html);
+                    });
+                }
+            });
+        }
+    });
 });
